93bc4d833c7535322bbab3fb04e62f51b718e382
dtaweb <darren.t.andrews@gmail.com>
2014-10-18 18:52:58 -0300

    My name is Inigo Montoya
    You killed my father. Prepare to die.

---
+++ unhidden-treasures/organizerLogin.php
@@ -0,0 +1,13 @@
+<?php
+
+	//page to login as previously-entered organizer
+?>
+Log in below:<br /><br >
+<form action="./authenticateOrganizer.php" method="POST">
+	Email: <input type="text" name="email" value="" /><br /><br />
+	Password: <input type="password" name="password1" value="" /><br /><br />
+	<input type="submit" name="submit" value="submit" />
+</form>
+<?php	//forgot password
+
+?>
